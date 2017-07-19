df27b9fada8158a21f070fb3acd21e202026053a
sakshi151990 <sakshikansal08@gmail.com>
2017-07-20 00:57:16 +1000

    spring security added

---
+++ SpitterApplication/src/main/java/spitter/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package spitter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvcSecurity
+public class SecurityConfig extends  WebSecurityConfigurerAdapter 	{
+ public void configure(AuthenticationManagerBuilder auth) throws Exception
+ {//with user will add the below users in inmemory user store
+	 auth.inMemoryAuthentication().withUser("user").password("password").roles("USER").and().withUser("ADMIN").password("password").roles("USER","ADMIN");
+ }
+ protected void configure(HttpSecurity http) throws Exception {
+ http
+ .authorizeRequests()
+ .antMatchers("/spitter/me").hasRole("SPITTER")
+ .antMatchers(HttpMethod.POST, "/spittles").hasRole("SPITTER")
+ .anyRequest().permitAll().and()
+ .requiresChannel()
+ .antMatchers("/spitter/form").requiresSecure().and().formLogin().loginPage("/login.html");
+ }}
+
---
+++ SpitterApplication/src/main/java/spitter/config/SecurityWebInitializer.java
@@ -0,0 +1,7 @@
+package spitter.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebInitializer  extends AbstractSecurityWebApplicationInitializer {
+
+}
